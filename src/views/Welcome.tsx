4d6ca459236c94c54f51056b8d0ad1f2bb17a9cd
曹珂俭 <1849201815@qq.com>
2022-11-19 18:17:44 +0800

    组件添加 第五个页面

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,14 +1,29 @@
 import { defineComponent, ref, Transition, VNode, watchEffect } from 'vue';
-import { RouteLocationNormalizedLoaded, RouterView } from 'vue-router';
+import { RouteLocationNormalizedLoaded, routerKey, RouterView,useRoute, useRouter } from 'vue-router';
 import { useSwipe } from '../hooks/useSwipe';
+import { throttle } from '../shared/throttle';
 import s from './Welcome.module.scss'
 export const Welcome = defineComponent({
   setup: (props, context) => {
     const main = ref<HTMLElement>()
-    // const { direction, swiping } = useSwipe(main)
-    // watchEffect(() => {
-    //   console.log(swiping.value, direction.value)
-    // })
+    const { direction, swiping } = useSwipe(main,{beforeStart: e => e.preventDefault()} )
+    const route =useRoute()
+    const router = useRouter()
+    const pushMap: Record<string,string> = {
+      'Welcome1':'/Welcome/2',
+      'Welcome2':'/Welcome/3',
+      'Welcome3':'/Welcome/4',
+      'Welcome4':'/start',
+    }
+    const push = throttle(()=>{
+      const name = (route.name || 'Welcome1').toString()
+      router.push(pushMap[name])
+    },500)
+    watchEffect(() => {
+      if(swiping.value && direction.value === 'left'){
+        push()
+      }
+    })
     return () => <div class={s.wrapper}>
       <header>
         <svg>
