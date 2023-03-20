e97edc795d453a4031229101d46227559bb5549c
曹珂俭 <1849201815@qq.com>
2023-03-20 17:12:17 +0800

    创建欢迎界面

---
+++ src/views/Welcome.tsx
@@ -0,0 +1,13 @@
+import { defineComponent, PropType } from 'vue';
+import { RouterView } from 'vue-router'
+import s from './Welcome.module.scss';
+export const Welcome = defineComponent({
+
+  setup: (props, context) => {
+    return () => (
+      <div>
+        <RouterView />
+      </div>
+    )
+  }
+})
