2f119b673cd40ed63cfe05d91ef0d85f4acee1f9
曹珂俭 <1849201815@qq.com>
2023-03-22 07:33:33 +0800

    修改svg用法

--- src/components/welcome/third.tsx
+++ src/components/welcome/third.tsx
@@ -5,7 +5,9 @@ import { FunctionalComponent } from 'vue';
 export const Third: FunctionalComponent = () => {
   return (
     <div class={s.card}>
-      <img class={s.icon} src={chart} />
+      <svg>
+        <use xlinkHref='#chart'></use>
+      </svg>
       <h2>数据可视化<br />收支一目了然</h2>
     </div>
   )
