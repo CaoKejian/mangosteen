067fe1528d6e15892220ce68ba8b4e28c28407d0
曹珂俭 <1849201815@qq.com>
2023-03-22 17:01:28 +0800

    完成overlay样式

--- src/shared/OverLay.tsx
+++ src/shared/OverLay.tsx
@@ -1,4 +1,5 @@
 import { defineComponent, PropType } from 'vue';
+import { RouterLink } from 'vue-router';
 import s from './OverLay.module.scss';
 export const OverLay = defineComponent({
   props: {
@@ -10,26 +11,33 @@ export const OverLay = defineComponent({
     const close = () => {
       props.onClose?.()
     }
+    const onClickSignIn = () => { }
     return () => (<>
       <div class={s.mask} onClick={close}></div>
       <div class={s.overlay}>
-        <section>
+        <section class={s.currentUser} onClick={onClickSignIn}>
           <h2>未登录用户</h2>
           <p>点击登录</p>
         </section>
         <nav>
-          <ul>
+          <ul class={s.action_list}>
             <li>
-              <svg class={s.svg}><use xlinkHref='#charts'></use></svg>
-              <span>统计图表</span>
+              <RouterLink to="/statistics" class={s.action}>
+                <svg class={s.icon}><use xlinkHref='#charts'></use></svg>
+                <span>统计图表</span>
+              </RouterLink>
             </li>
             <li>
-              <svg class={s.svg}><use xlinkHref='#export'></use></svg>
-              <span>导出数据</span>
+              <RouterLink to="/export" class={s.action}>
+                <svg class={s.icon}><use xlinkHref='#export'></use></svg>
+                <span>导出数据</span>
+              </RouterLink>
             </li>
             <li>
-              <svg class={s.svg}><use xlinkHref='#notify'></use></svg>
-              <span>记账提醒</span>
+              <RouterLink to="/notify" class={s.action}>
+                <svg class={s.icon}><use xlinkHref='#notify'></use></svg>
+                <span>记账提醒</span>
+              </RouterLink>
             </li>
           </ul>
         </nav>
