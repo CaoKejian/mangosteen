c412f34604e09bf653233145f9b783afe7586b68
曹珂俭 <1849201815@qq.com>
2023-03-22 10:51:42 +0800

    提交Button小bug

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -1,7 +1,7 @@
 import { defineComponent, PropType } from 'vue';
 import s from './Button.module.scss';
 interface X {
-  onClick: (e: Event) => void
+  onClick?: (e: Event) => void
 }
 export const Button = defineComponent<X>({
   setup: (props, context) => {
