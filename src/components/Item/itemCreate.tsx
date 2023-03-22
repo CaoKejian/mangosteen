9b75fa027e97a9b0b32911a1a864a7f3c4270808
曹珂俭 <1849201815@qq.com>
2023-03-22 17:33:30 +0800

    正在实现itemCreate页面

--- src/components/Item/itemCreate.tsx
+++ src/components/Item/itemCreate.tsx
@@ -1,15 +1,22 @@
-import { defineComponent, PropType} from 'vue';
+import { defineComponent, PropType } from 'vue';
+import { MainLayout } from '../../layouts/MainLayout';
 import s from './itemCreate.module.scss';
-export const itemCreate= defineComponent({
-  props:{
-    name:{
-      type:String as PropType<string>
+export const itemCreate = defineComponent({
+  props: {
+    name: {
+      type: String as PropType<string>
     }
   },
-  setup:(props,context)=>{
-    return ()=>(
-        <div>
-        </div>
+  setup: (props, context) => {
+    return () => (
+      <MainLayout class={s.main}>{
+        {
+          title: () => '记一笔',
+          icon: () => <svg class={s.svg}><use xlinkHref='#return'></use></svg>,
+          default: () =>
+            <div>1</div>
+        }
+      }</MainLayout>
     )
   }
 })
--- src/views/ItemPage.tsx
+++ src/views/ItemPage.tsx
@@ -1,11 +1,11 @@
 import { defineComponent, PropType } from 'vue';
+import { RouterView } from 'vue-router';
+import { MainLayout } from '../layouts/MainLayout';
 import s from './ItemPage.module.scss';
 export const ItemPage = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div>
-        1
-      </div>
+      <RouterView></RouterView>
     )
   }
 })
