047a25a9cad171706e9f7b295f126acd5005c1b6
曹珂俭 <1849201815@qq.com>
2023-04-06 15:54:02 +0800

    创建mock模块

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -42,8 +42,10 @@ export const SignInPage = defineComponent({
       ]
       Object.assign(errors, validate(formData, reules))
       if (!hasError(errors)) {
-        const response = await http.post<{ jwt: string }>('/session', formData)
-          .catch(onError)
+        const response = await http.post<{ jwt: string }>('/session', formData, {
+          params: { _mock: 'session' }
+        }).catch(onError)
+        console.log(response)
         localStorage.setItem('jwt', response.data.jwt)
         /*
           第一种方式 localstorage
