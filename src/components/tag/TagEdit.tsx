af28e243288cf43ab174d715052cceb031d4adfd
曹珂俭 <1849201815@qq.com>
2023-04-07 15:58:41 +0800

    删除标签

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -4,7 +4,9 @@ import { MainLayout } from '../../layouts/MainLayout';
 import s from './Tag.module.scss';
 import { TagForm } from './TagForm';
 import { BackIcon } from '../../shared/BackIcon';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
+import { http } from '../../shared/Http';
+import { Dialog } from 'vant';
 export const TagEdit = defineComponent({
   setup: (props, context) => {
     const route = useRoute()
@@ -14,6 +16,20 @@ export const TagEdit = defineComponent({
         <div>id 不存在</div>
       }
     }
+    const router = useRouter()
+    const onDelete = async(options?: {withItems?: boolean} ) =>{
+      await Dialog.confirm({
+        title:"提示",
+        message:"确认删除吗？"
+      })
+      await http.delete(`/tags/${numberId}`,{
+        withItems: options?.withItems? 'true' : 'false'
+      })
+      router.back()
+    }
+    const onDeleteHard = () =>{
+
+    }
     return () => (
       <MainLayout>{{
         title: () => '编辑标签',
@@ -21,8 +37,8 @@ export const TagEdit = defineComponent({
         default: () => <>
           <TagForm id={numberId} />
           <div class={s.actions}>
-            <Button level='danger' class={s.removeTags} onClick={() => { }}>删除</Button>
-            <Button level='danger' class={s.removeTagsAndItems} onClick={() => { }}>删除标签和记账</Button>
+            <Button level='danger' class={s.removeTags} onClick={() =>onDelete()}>删除标签</Button>
+            <Button level='danger' class={s.removeTagsAndItems} onClick={()=>onDelete({withItems: true})}>删除标签和记账</Button>
           </div>
         </>
       }}</MainLayout>
