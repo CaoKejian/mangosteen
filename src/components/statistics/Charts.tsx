9eee671e91cad99acc70b40eb8015a8e06c8782c
曹珂俭 <1849201815@qq.com>
2023-04-12 07:55:16 +0800

    完成Bar

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -8,6 +8,7 @@ import { LineChart } from '../echarts/LineChart';
 import { Time } from '../../shared/time';
 import { PieChart } from '../echarts/PieChart';
 import { BallChart } from '../echarts/BallChart';
+import { Bars } from '../echarts/Bars';
 
 type Data1Item = { happen_at: string, amount: number }
 type Data1 = Data1Item[]
@@ -56,23 +57,10 @@ export const Charts = defineComponent({
       })
       //  data1.value.slice(-2)[0]
       data1.value = response.data.groups
-      console.log(response.data.groups);
+    })
 
-    })
-    const data3 = reactive([
-      { tag: { id: 1, name: '房租', sign: 'x' }, amount: 3000 },
-      { tag: { id: 2, name: '吃饭', sign: 'x' }, amount: 1000 },
-      { tag: { id: 3, name: '娱乐', sign: 'x' }, amount: 900 },
-    ])
-    const betterData3 = computed(() => {
-      const total = data3.reduce((sum, item) => sum + item.amount, 0)
-      return data3.map(item => ({
-        ...item,
-        percent: Math.round(item.amount / total * 100) + '%'
-      }))
-    })
     const data2 = ref<Data2>([])
-    
+
     const betterData2 = computed<{ name: string; value: number }[]>(() =>
       data2.value.map((item) => ({
         name: item.tag.name,
@@ -89,6 +77,13 @@ export const Charts = defineComponent({
       })
       data2.value = response.data.groups
     })
+    const betterData3 = computed<{ tag: Tag; amount: number; percent: number }[]>(() => {
+      const total = data2.value.reduce((sum, item) => sum + item.amount, 0)
+      return data2.value.map((item) => ({
+        ...item,
+        percent: Math.round((item.amount / total) * 100)
+      }))
+    })
     return () => (
       <div class={s.wrapepr}>
         <FormItem label='类型' type='select'
@@ -101,27 +96,7 @@ export const Charts = defineComponent({
         <LineChart data={betterData1.value} />
         <PieChart data={betterData2.value} />
         <BallChart />
-        <div ref={refDiv3} class={s.demo3}></div>
-        <div class={s.demo3}>
-          {betterData3.value.map(({ tag, amount, percent }) => {
-            return (
-              <div class={s.topItem}>
-                <div class={s.sign}>
-                  {tag.sign}
-                </div>
-                <div class={s.bar_wrapper}>
-                  <div class={s.bar_text}>
-                    <span> {tag.name} - {percent} </span>
-                    <span> ￥{amount} </span>
-                  </div>
-                  <div class={s.bar}>
-                    <div class={s.bar_inner}></div>
-                  </div>
-                </div>
-              </div>
-            )
-          })}
-        </div>
+        <Bars data={betterData3.value} />
       </div>
     )
   }
