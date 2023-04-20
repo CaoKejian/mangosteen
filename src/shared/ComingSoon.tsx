45fdf52e49d8bcc7648905952608a2ea7a5ba934
曹珂俭 <1849201815@qq.com>
2023-04-20 17:39:29 +0800

    敬请期待添加返回

--- src/shared/ComingSoon.tsx
+++ src/shared/ComingSoon.tsx
@@ -1,4 +1,6 @@
 import { defineComponent, PropType } from 'vue'
+import { useRouter } from 'vue-router'
+import { Button } from './Button'
 import s from './ComingSoon.module.scss'
 export const ComingSoon = defineComponent({
   props: {
@@ -7,6 +9,10 @@ export const ComingSoon = defineComponent({
     }
   },
   setup: (props, context) => {
+    const router = useRouter()
+    const onClick = () => {
+      router.back()
+    }
     return () => (
       <div>
         <div class={s.svg_wrapper}>
@@ -15,6 +21,9 @@ export const ComingSoon = defineComponent({
           </svg>
         </div>
         <p class={s.text}>敬请期待</p>
+        <p class={s.link} >
+          <Button onClick={onClick}>返回</Button>
+        </p>
       </div>
     )
   }
