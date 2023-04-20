ba71e79edefd502a8760024937feee755d2965b8
曹珂俭 <1849201815@qq.com>
2023-04-20 18:57:42 +0800

    撤销上次提交

--- vite.config.ts
+++ vite.config.ts
@@ -43,7 +43,7 @@ export default defineConfig(({command}) => {
     server: {
       proxy: {
         '/api/v1': {
-          target: 'https://mangosteen.caokejian.club/api/v1'
+          target: 'http://8.130.24.249:3000/'
         }
       }
     }
